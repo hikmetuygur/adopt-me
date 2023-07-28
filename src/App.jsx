0ff3cc8240d46b1e5434b3ce941e758047f670d1
Hikmet Uygur <uygurhikmet@gmail.com>
2023-07-28 18:52:30 +0300

    Mapping through data with hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me !</h1>
-      <Pet name="Rıfkı" animal="Kuş" breed="Muhabbet Kuşu" />
-      <Pet name="Hüsnü" animal="Kuş" breed="Muhabbet Kuşu" />
-      <Pet name="Turşu" animal="Kuş" breed="Muhabbet Kuşu" />
+      <SearchParams />
     </div>
   );
 };
